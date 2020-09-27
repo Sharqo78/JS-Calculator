88fb981b2e68bfbe5f0eac9c447bb3b331f945f9
sharqo78 <salime3ccollege@gmail.com>
2020-09-27 16:07:47 +0200

    J'ai réupload le repo parce que je l'avais mal crée dès le début

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS Calculator
+
+Ce projet est le premier projet en JavaScript qui nous a été demandé de faire en BTS Systèmes Numériques
+C'est une calculatrice très simple qui effectue 4 opérations différentes lorsqu'on lui donne 2 nombres dans chacune de ses cases.
+Bien évidemment j'ai passé beaucoup plus de temps sur le CSS qu'autre chose, ce qui me semble logique
---
+++ fonction.js
@@ -0,0 +1,20 @@
+
+function calculs(){
+let addition
+let val1 = parseFloat(document.getElementById("valeur1").value);
+let val2 = parseFloat(document.getElementById("valeur2").value);
+addition = val1 + val2;
+document.getElementById("resultAdd").textContent = "Addition : "  + addition;
+soustraction = val1-val2;
+document.getElementById("resultSub").textContent = "Soustraction : "  + soustraction;
+multiplication = val1*val2;
+document.getElementById("resultMult").textContent = "Multiplication : "  + multiplication;
+division = val1/val2;
+document.getElementById("resultDiv").textContent = "Divison : "  +  division;
+}
+
+window.onload = function(){
+    var button = document.getElementById('valider');
+    button.form.submit();
+}
+
